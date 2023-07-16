6531718ff15400c30655987d6b7c3315aad3f975
MohamedAhme-11 <81529376+MohamedAhme-11@users.noreply.github.com>
2023-07-16 03:46:16 +0300

    Add files via upload

---
+++ context.py
@@ -0,0 +1,67 @@
+from smtplib import SMTP_SSL # while using gmail needs smtp to make connection , ssl to make it be secure 
+from email.message import EmailMessage # for desging the email structure 
+import os #using os power
+import json #for using json file 
+from mimetypes import guess_type #to get the type for every file i using 
+
+ 
+
+
+json_file = open("email.json") # read the json file 
+data_from_json_file = json.load(json_file) #load the data in the json file
+
+SENDER_EMAIL_sender = data_from_json_file["email"] #taking email info from data loaded
+EMAIL_PASSWORD_sender = data_from_json_file["password"] #taking password info from data loaded
+files_to_send = input("please enter the file")# loading all the files with all files type
+ 
+
+
+
+def Send_Mail(SENDER_EMAIL_sender, RECEIVER_EMAIL, EMAIL_PASSWORD_sender, SUBJECT, CONTENT, files_to_send):# function for sending the emails and receive all the email info 
+    #formating my emails
+    msg = EmailMessage()# create email message class for formating the email
+    msg["From"] = SENDER_EMAIL_sender# saying the sender email
+    msg["To"] = RECEIVER_EMAIL# saying the receiver email
+    msg["Subject"] = SUBJECT# the email subject. the title of the email
+    msg.set_content(CONTENT)# the email content 
+    
+    mime_type, encoding = guess_type(files_to_send)# will get from this line what type of data for every file 
+    app_type, sub_type = mime_type.split("/")[0], mime_type.split("/")[1]#mime type the file type afterr the ""/" and the . , #apptype # the file is image or exceel and so on.. 
+
+    with open(files_to_send, "rb") as binray_file:# open the file 
+        file_data = binray_file.read()#read the file content
+            # add attchemnt with type of data you are sending, app type like ex.image,sub type like ex.png
+        msg.add_attachment(
+        file_data, maintype=app_type, subtype=sub_type, filename=files_to_send
+                )
+        
+
+
+ 
+
+    
+    
+
+
+
+
+
+    #sending mail via smtp server #make connection
+    with SMTP_SSL("smtp.gmail.com", 465) as smtp:#using the gmail and make connection with port number
+        smtp.login(SENDER_EMAIL_sender, EMAIL_PASSWORD_sender)#login 
+        smtp.send_message(msg)#sending the message created with its structure
+        smtp.close()#close the connection 
+    print("mail sent sucessfully")#after sending the mail sucessfully printing mail send sucessfully
+
+
+RECEIVER_EMAIL = input("please enter the receiver email :")#reciver email
+SUBJECT = "Test"# email subject
+## email content
+CONTENT = """ 
+HELLO 
+
+
+This mail for you just for test ,
+Thanks and regrads
+"""
+Send_Mail(SENDER_EMAIL_sender, RECEIVER_EMAIL, EMAIL_PASSWORD_sender, SUBJECT, CONTENT, files_to_send)#calling the function
---
+++ sending email.md
@@ -0,0 +1,22 @@
+# Send Email using Python  📧
+
+**What is it?** 🤨
+
+Sending email with a Python script is the easiest way for users to quickly send emails and attach files.
+
+**Why should I make it?** 🧐
+
+Download the project and run the default environment from the Scripts file and then run the context.py file and insert the file you will attach it to in the email and enter the email for the  person who will receive it.
+
+**Who should make it?** ✋
+
+Anyone who is working on a programming project, especially if you want others to use it or contribute.
+
+**How should I make it?** 👨‍💻
+
+there is a lot of python models for sending emails you can see in github but this model is different that you can input the email user for who you want send to the email
+and can input any file path or file name you want  to send 
+
+
+
+
